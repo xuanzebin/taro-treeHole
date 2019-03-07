a358b6647a0e1818da336ce631349e4632dde2bb
xuanzebin <xuanzebin@126.com>
2019-03-07 16:57:57 +0800

    新增了用户页面的下拉刷新功能

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -10,11 +10,32 @@ import UserCard from '../../components/userCard/userCard'
 class User extends Component {
 
   config = {
-    navigationBarTitleText: '个人中心'
+    navigationBarTitleText: '个人中心',
+    backgroundTextStyle: 'dark',
+    enablePullDownRefresh: true,
   }
   constructor() {
     super(...arguments)
-    this.state = {}
+    this.state = {
+      shouldRefresh: true
+    }
+  }
+  onPullDownRefresh() {
+    const { treeHoleStore } = this.props
+    let shouldRefresh = this.state.shouldRefresh
+    if (shouldRefresh) {
+      shouldRefresh = false
+      Taro.startPullDownRefresh()
+      this.setState({
+        shouldRefresh: false
+      })
+      treeHoleStore.fetchMessageList().then(() => {
+        Taro.stopPullDownRefresh()
+        this.setState({
+          shouldRefresh: true
+        })
+      })
+    }
   }
   render() {
     return (
